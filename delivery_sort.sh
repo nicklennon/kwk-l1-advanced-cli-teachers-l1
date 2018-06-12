346045c169876935171a35a63cc3d50d652a9245
nicklennon <nicklennon.53@gmail.com>
2018-06-12 01:31:52 +0000

    automatically backed up by learn

--- delivery_sort.sh
+++ delivery_sort.sh
@@ -1,5 +1,8 @@
 ls;
-mv delivery apples.txt fruit;
-mv delivery grapples.txt fruit;
-mv delivery tomatoes.txt fruit;
-mv delivery bison_burgers.txt meat;
+mv delivery/apples.txt fruit;
+mv delivery/grapples.txt fruit;
+mv delivery/tomatoes.txt fruit;
+mv delivery/bison_burgers.txt meat;
+mv delivery/hotdogs.txt meat;
+mv delivery/lamb_ribs.txt meat;
+mv delivery/brie.txt dairy;
