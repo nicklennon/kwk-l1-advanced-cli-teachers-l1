2a3bd211e9ca3e7a5419ffc850252def9ef1f62c
nicklennon <nicklennon.53@gmail.com>
2018-06-12 01:22:41 +0000

    automatically backed up by learn

--- test_script.sh
+++ test_script.sh
@@ -3,4 +3,4 @@ mkdir this_is_a_directory_made_in_a_script;
 mkdir another_directory;
 touch this_is_a_directory_made_in_a_script/first_file.txt;
 this_is_a_directory_made_in_a_script/second_file.txt;
-
+mv this_is_a_directory_made_in_a_script/second_file.txt another_directory;
